54946d3035168bfcac46543216a90d062b72b770
Florian Regente <Florian@Florians-MacBook-Pro.fritz.box>
2015-12-02 23:29:45 +0100

    add images to tile Pane

--- src/fotogallerie/javafx/FXMLDocumentController.java
+++ src/fotogallerie/javafx/FXMLDocumentController.java
@@ -5,7 +5,10 @@
  */
 package fotogallerie.javafx;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,8 +17,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import javafx.stage.DirectoryChooser;
 
 /**
  *
@@ -23,16 +28,47 @@ import javafx.scene.layout.TilePane;
  */
 public class FXMLDocumentController implements Initializable {
     
+    private final List<File> imageList = new ArrayList();
+    
     @FXML
-    private Pane imageGridPane;
+    private TilePane imageGridPane;
+    @FXML
+    private ScrollPane imageScrollPane;
     
     
     @FXML
     private void openFolder(ActionEvent event) {
-        System.out.println("Open Folder");
         
+        
+        DirectoryChooser dicChooser = new DirectoryChooser();
+        File file = dicChooser.showDialog(null);
+        addFilesFromDir(file);
+        
+        for(int i =0 ; i <imageList.size();i++){
+            System.out.println("Open Folder: "+ " "+ imageList.get(i));
+            
+            ImageView smallImageView = new ImageView();
+            Image image = new Image(imageList.get(i).toURI().toString());
+            smallImageView.setImage(image);
+            System.out.println(image);
+            smallImageView.setFitWidth(100);
+            smallImageView.setFitHeight(100);
+           
+            imageGridPane.getChildren().add(smallImageView);
+            
+        }
         
     }
+    
+     private void addFilesFromDir(File dir) {
+        if (dir.isDirectory()) {
+            File[] fileList = dir.listFiles();
+            for (File file : fileList) {
+                imageList.add(file);
+            }
+        }
+    }
+    
     @FXML
     private void openImage(ActionEvent event) {
         System.out.println("Open Image");
--- src/fotogallerie/javafx/FotogallerieJavaFX.java
+++ src/fotogallerie/javafx/FotogallerieJavaFX.java
@@ -5,6 +5,9 @@
  */
 package fotogallerie.javafx;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
